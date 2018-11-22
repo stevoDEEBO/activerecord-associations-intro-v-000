9e2e2b76011fbea9440debac686d5742ae8c787e
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-11-22 17:14:56 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,12 @@
 
 ActiveRecord::Schema.define(version: 4) do
 
-  create_table "songs", force: :cascade do |t|
+  create_table "artists", force: :cascade do |t|
     t.string "name"
-    t.integer "artist_id"
-    t.integer "genre_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
   end
 
 end
